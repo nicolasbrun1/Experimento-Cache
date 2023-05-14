a791d2529bc376fc59edc95b23f107215f5338d4
Nicolas Brun <63732397+nicolasbrun1@users.noreply.github.com>
2023-05-14 20:34:39 -0300

    Mapeamento Direto concluído

--- Trabalho Ciber/mapeamento-direto.py
+++ Trabalho Ciber/mapeamento-direto.py
@@ -19,6 +19,34 @@ def imprimir_cache(cache):
 imprimir_cache(cache)
 
 def mapeamento_direto(tamanho_cache, pos_memoria):
-    pos_memoria = range(cache)
-    tamanho_cache
-    pos_cache = pos_memoria % tamanho_cache
+
+    inicializar_cache(tamanho_cache)
+
+    imprimir_cache(cache)
+
+    miss = 0
+    hit = 0
+    pos_memoria
+    loop = 0
+    for x,pos in enumerate(pos_memoria):
+
+        loop += 1
+        
+        if cache[pos % tamanho_cache] == pos:
+            hit += 1
+            print('STATUS: Hit')
+        else:
+            miss += 1
+            print('STATUS: Miss')
+            cache[pos % tamanho_cache] = pos
+
+        print('-----------------')
+        imprimir_cache(cache)
+        print(f'Total de Hits:{hit}')
+        print(f'Total de Miss:{miss}')
+    print(f'Total de posições acessadas:{loop}')
+    print(f'Taxa de hits:{(hit%miss + hit) * 100}')
+
+posicoes_memoria_acessar = [33,33,5,5]
+tamanho_cache = 5
+mapeamento_direto(tamanho_cache, posicoes_memoria_acessar)
